1990ef96917454e3003f42fb695636d84fcf2faa
SadisticSun <tristan@iamt.nl>
2017-03-21 12:39:08 +0100

    Added API calls

--- app.js
+++ app.js
@@ -1,5 +1,34 @@
-var express = require('express');
-var app = express();
+const express = require('express');
+const request = require('request');
+const bodyParser = require('body-parser');
+const app = express();
+
+const urlencodedParser = bodyParser.urlencoded({ extended: false });
+
+const APIconfig = {
+    KEY: 'dc6zaTOxFJmzC',
+    LIMIT: 25
+};
+
+function doApiCall(query, key, limit) {
+
+    var url = 'https://api.giphy.com/v1/gifs/search?q=' + query + '&api_key=' + key + '&limit=' + limit;
+    console.log(url);
+
+    request(url, function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+            var parsedData = JSON.parse(body);
+            parsedData.data.forEach(function (obj) {
+                console.log(obj.url);
+            })
+        } else {
+            console.log('Er ging iets mis met het ophalen van de data uit de API...')
+        }
+    });
+}
+
+app.use(urlencodedParser);
+// app.use(bodyParser.json());
 
 app.set('view engine', 'ejs');
 
@@ -14,9 +43,20 @@ app.get('/about', function (req, res) {
 });
 
 app.get('/search', function (req, res) {
-    res.render('search')
+    res.render('search', {qs: req.query});
 });
 
-app.listen(3000, function () {
-    console.log('Example app listening on: http://localhost:3000!')
+app.post('/search', urlencodedParser, function(req, res) {
+    var userQuery = req.body.query;
+    console.log(req.body.query);
+    res.render('search', {qs: req.body});
+    doApiCall(userQuery, APIconfig.KEY, APIconfig.LIMIT);
+
 });
+
+
+app.listen(3000, function () {
+    console.log('App listening on: http://localhost:3000')
+});
+
+
