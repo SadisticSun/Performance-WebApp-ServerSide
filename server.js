41581301ae2281b067e990f3460c3c74d5babc0e
SadisticSun <tristan@iamt.nl>
2017-03-23 21:18:21 +0100

    Beautified code

--- server.js
+++ server.js
@@ -1,40 +1,31 @@
 const express = require('express');
 const request = require('request');
 const bodyParser = require('body-parser');
-
 const app = express();
-const urlencodedParser = bodyParser.urlencoded({ extended: false });
-
+const urlencodedParser = bodyParser.urlencoded({
+    extended: false
+});
 const APIconfig = {
     KEY: 'dc6zaTOxFJmzC',
     LIMIT: 20
 };
-
 app.use(urlencodedParser);
-
 app.set('view engine', 'ejs');
-
 app.use(express.static('public'));
-
-app.get('/', function (req, res) {
+app.get('/', function(req, res) {
     res.render('index')
 });
-
-
-app.get('/search', function (req, res) {
+app.get('/search', function(req, res) {
     var data = {};
     res.render('search', {
         user_query: req.query,
         items: null
     });
 });
-
 objects = [];
-
 app.post('/search', urlencodedParser, function(req, res) {
     var userQuery = req.body.query;
-    doApiCall(userQuery, APIconfig.KEY, APIconfig.LIMIT, function (data) {
-
+    doApiCall(userQuery, APIconfig.KEY, APIconfig.LIMIT, function(data) {
         objects.push(data.data);
         // console.log('objects[] bevat nu: ' + objects);
         res.render('search', {
@@ -42,30 +33,21 @@ app.post('/search', urlencodedParser, function(req, res) {
             items: data.data
         });
     });
-
 });
-
-app.get('/details/:id', function (req, res) {
+app.get('/details/:id', function(req, res) {
     res.render('details', {
-        single_img_url: 'https://media4.giphy.com/media/'+req.params.id+'/200.gif'
+        single_img_url: 'https://media4.giphy.com/media/' + req.params.id + '/200.gif'
     });
 });
-
-
-app.listen(8080, function () {
+app.listen(8080, function() {
     console.log('App listening on: http://localhost:8080')
 });
 
-
 function doApiCall(query, key, limit, callback) {
-
     var url = 'https://api.giphy.com/v1/gifs/search?q=' + query + '&api_key=' + key + '&limit=' + limit;
     console.log("URL is: " + url);
-
-    request(url, function (error, response, body) {
+    request(url, function(error, response, body) {
         var parsedData = JSON.parse(body);
         callback(parsedData)
     });
 }
-
-
