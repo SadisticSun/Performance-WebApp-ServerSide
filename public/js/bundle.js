5daf016e06f26f74111182f97f0596041d4f1b10
SadisticSun <tristan@iamt.nl>
2017-03-31 14:47:35 +0200

    async css function

--- public/js/bundle.js
+++ public/js/bundle.js
@@ -2,6 +2,33 @@
 (function(){
     "use strict";
 
+    var loadCSS = function(href, before, media){
+      var ss = window.document.createElement('link');
+      var ref = before || window.document.getElementsByTagName('script')[0];
+      var sheets = window.document.styleSheets;
+      ss.rel =  'stylesheet';
+      ss.href = href;
+      ss.media = 'only x';
+      ref.parentNode.insertBefore(ss, ref);
+      function toggleMedia() {
+          var defined;
+          for (var i = 0; i < sheets.length; i++) {
+              if (sheets[i].href && sheets[i].href.indexOf(href) > -1) {
+                  defined = true;
+              }
+          }
+          if (defined) {
+              ss.media = media || 'all';
+          }
+          else {
+              setTimeout(toggleMedia);
+          }
+      }
+      toggleMedia();
+      return ss;
+    };
+    loadCSS( "./css/main.css" );
+
     var utils = {
         $: function(id) {
             return document.querySelector(id);
